821a7beeee9617685fc84b963bf169bdc396f853
Your Name <you@example.com>
2018-09-22 20:44:39 +0800

    111

--- resources/views/home/searching/searchTa.blade.php
+++ resources/views/home/searching/searchTa.blade.php
@@ -18,15 +18,16 @@
 
         @foreach($items['people'] as $item)
         <div class="am-g">
-            <div class="am-u-sm-12">
+            <div class="am-u-sm-12" style="margin-top: 10px;">
                 <div class="am-thumbnail">
                     <img src="{{'uploads/' . \App\Member::find($item->user_id)->image}}" alt="" class="am-comment-avatar" width="48"
                          height="48"/>
+                    <span>{{\App\Member::find($item->user_id)->name}}</span>
                     <div class="am-thumbnail-caption">
-                        <p>{{\App\Member::find($item->user_id)->name}}</p>
+
                         <p>{{$item->item_name}}</p>
-                        <p>{{$item->item_detail}}</p>
-                        <p>{{date('Y.m.d',strtotime($item->created_at))}}</p>
+                        <p>待寻人描述：{{$item->item_detail}}</p>
+                        <p>发布时间：{{date('Y.m.d',strtotime($item->created_at))}}</p>
                         <p>
                             <a href="{{route('findpeople.show',$item->id)}}" class="btn btn-success" role="button">
                                 查看详情
