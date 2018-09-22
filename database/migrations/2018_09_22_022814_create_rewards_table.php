3fb0e3b9e757eb48271741e6e9a350516ca7fae8
huhuapin <867840692@qq.com>
2018-09-22 10:34:32 +0800

    建好了表

---
+++ database/migrations/2018_09_22_022814_create_rewards_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRewardsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rewards', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('give_id');  //打赏者id
+            $table->integer('accept_id'); //被打赏者id
+            $table->integer('num');  //积分数值
+            $table->integer('type')->nullable(); //打赏类型  1问题打赏  2故事打赏
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rewards');
+    }
+}
