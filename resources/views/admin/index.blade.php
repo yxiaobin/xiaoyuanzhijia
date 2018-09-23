e4c0a1ac29249954f128fad0c8eefe131def5958
huhuapin <867840692@qq.com>
2018-09-23 11:43:37 +0800

    修改后台模板界面

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -16,25 +16,25 @@
 
         </div>
         <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
-            <a class="card card-banner card-yellow-light" href="{{url('admin/new/create')}}">
+            <a class="card card-banner card-yellow-light" href="{{url('admin/goods')}}">
                 <div class="card-body">
                     <i class="icon fa fa-plus-square fa-4x"></i>
 
                     <div class="content">
                         <div class="title" href=""></div>
-                        <div class="value"><span class="sign">发布新闻</span></div>
+                        <div class="value"><span class="sign">添加商品</span></div>
                     </div>
                 </div>
             </a>
         </div>
         <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
-            <a class="card card-banner card-yellow-light" href="{{url('admin/product/create')}}">
+            <a class="card card-banner card-yellow-light" href="{{url('admin/repair')}}">
                 <div class="card-body">
                     <i class="icon fa fa-plus-square fa-4x"></i>
 
                     <div class="content">
                         <div class="title" href=""></div>
-                        <div class="value"><span class="sign">发布新产品</span></div>
+                        <div class="value"><span class="sign">报修管理</span></div>
                     </div>
                 </div>
             </a>
@@ -90,39 +90,5 @@
                 </div>
             </div>
         </div>
-        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-            <div class="card card-mini">
-                <div class="card-header">
-                    <div class="card-title">软件信息</div>
-                </div>
-                <div class="card-body no-padding table-responsive">
-                    <table class="table">
-                        <tbody>
-                        <tr>
-                            <td>软件开发：</td>
-                            <td>山东萌芽网络科技有限公司</td>
-                        </tr>
-                        <tr>
-                            <td>授权使用：</td>
-                            <td>山东和发环保科技有限公司</td>
-                        </tr>
-                        <tr>
-                            <td>名称：</td>
-                            <td>萌芽网站管理系统</td>
-                        </tr>
-                        <tr>
-                            <td>版本：</td>
-                            <td>2.0.1</td>
-                        </tr>
-                        <tr>
-                            <td>网址：</td>
-                            <td><a href="http://www.mengyakeji.com" target="_blank">www.mengyakeji.com</a></td>
-                        </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        </div>
-    </div>
 @endsection
 
